f223b16a323163c1d8c6ca155443b4ba7f2448cd
rbkpro <militooo9@gmail.com>
2015-07-30 15:38:03 +0100

    Commit injection class

---
+++ src/main/java/org/rbkpro/javacf/messenger/resources/InjectionDemoResource.java
@@ -0,0 +1,28 @@
+package org.rbkpro.javacf.messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/injection")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectionDemoResource {
+	
+	@GET
+	@Path("annotation")
+
+	public String getPramUsingAnnotation(@MatrixParam("param") String matParam,
+										 @HeaderParam("myValue") String valueParam,
+										 @CookieParam("name") String cookPram){
+		
+		return "matrix param : "+matParam+" / header costume param : "+valueParam+" / cookie param : "+cookPram;
+	}
+
+}
